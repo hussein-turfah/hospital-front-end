5de3c60c17ec42c4d28a00655d55a73d43b2ada6
Hussein-Turfah <77545041+Hussein-Turfah@users.noreply.github.com>
2023-03-15 21:46:21 +0200

    Added all js to the same script file except for the admin panel

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@ window.onload = () => {
   const register_btn = document.getElementById("register-button");
   register_btn.addEventListener("click", register);
 
+  const login_button = document.getElementById("login-button");
+  login_button.addEventListener("click", login);
+  
   function register(){
 
     let email = document.getElementById("email").value;
@@ -61,6 +64,34 @@ window.onload = () => {
     
 
   }
+  function login(){
 
-  
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    
+    let data = new FormData();
+    data.append("email", email);
+    data.append("password", password);    
+
+    axios
+      .post(base_url + 'login_api.php/', data)
+      .then((result) => {
+        if (result.data.user_id) {
+          sessionStorage.setItem("user_id", result.data.user_id);
+          sessionStorage.setItem("usertype_id", result.data.usertype_id);
+          let usertype_id = parseInt(result.data.usertype_id);
+          if(result.data.usertype_id == 3){
+            window.location.href = "../index.html"
+          }else if(result.data.usertype_id == 2){
+            window.location.href = "../pages/employees.html"
+          }else if(result.data.usertype_id == 1){
+            window.location.href = "../pages/admin_panel.html"
+            console.log('admin')
+          }
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
 };
